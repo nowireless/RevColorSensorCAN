325604b302199db8b3eb96cdb89a84ed2f4db7c8
Ryan Sjostrand <ryansjostrand@gmail.com>
2022-05-11 19:44:19 -0500

    Add CAN library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
+#include <CAN.h>
+
 void setup() {
-// write your initialization code here
+    // write your initialization code here
+    Serial.begin(9600);
 }
 
 void loop() {
-// write your code here
+    // write your code here
+    Serial.println("Hello");
+    delay(1000);
 }
